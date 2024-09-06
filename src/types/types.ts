e09cc0185b96ac98443167d1770900593da05d32
christina <chrisas0913@gmail.com>
2024-09-06 19:34:56 +0900

    setting type.ts document

---
+++ src/types/types.ts
@@ -0,0 +1,29 @@
+
+
+export interface Task {
+    id: string;
+    taskTitle: string;
+    taskStatus: string;
+    taskAssign: string[]|[];
+    taskNotAssign: string[]|[];
+    taskDate: string;
+    taskDescription: string;
+    taskOwner: string | null;
+    calendarId: string;
+    projectId: string;
+    projectTitle: string;
+    createdAt: string;  
+}
+
+
+export interface Project {
+    id: string;
+    uid: string;
+    projectTitle: string;
+    projectStatus: string;
+    projectMember: string[];
+    projectDateStart: string;
+    projectDateEnd: string;
+    projectOwner: string | undefined;
+    createdAt: string;
+}
